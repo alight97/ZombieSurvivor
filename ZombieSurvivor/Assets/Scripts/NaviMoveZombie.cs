28441d6d765e9a712f2ffa42a3cbba6b67229bd7
alight97 <alight97@naver.com>
2025-04-01 18:28:09 +0900

    좀비 점프 구현
    네비 메시 브릿지와 점프 에니메이션

--- ZombieSurvivor/Assets/Scripts/NaviMoveZombie.cs
+++ ZombieSurvivor/Assets/Scripts/NaviMoveZombie.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.AI.Navigation;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class NaviMoveZombie : MonoBehaviour
 {
     public float findDelay = 1.0f;
-    public float seekRange = 5.0f;
+    public float seekRange = 50.0f;
     public LayerMask targetLayerMask;
 
     private float lastFindTime;
     private GameObject naviTarget;
     private NavMeshAgent navMeshAgent;
     private Animator animator;
+
+    public List<NavMeshLink> navMeshLinkList;
+    public float jumpTime = 1.0f;
+    public float jumpHeight = 2.0f;
+    private float jumpStartTime;
+    private bool isJump;
+
 
     private void Awake()
     {
@@ -42,6 +50,17 @@ public class NaviMoveZombie : MonoBehaviour
 
     private void UpdateMove()
     {
+        if (isJump)
+        {
+            return;
+        }
+
+        if (navMeshAgent.isOnOffMeshLink)
+        {
+            StartCoroutine(JumpRoutine());
+            return;
+        }
+
         if (naviTarget != null)
         {
             navMeshAgent.isStopped = false;
@@ -54,6 +73,40 @@ public class NaviMoveZombie : MonoBehaviour
             navMeshAgent.isStopped = true;
 
             animator.SetFloat("Move", 0);
+        }
+    }
+
+    private IEnumerator JumpRoutine()
+    {
+        var startPos = navMeshAgent.currentOffMeshLinkData.startPos;
+        var endPos = navMeshAgent.currentOffMeshLinkData.endPos;
+
+        while (true)
+        {
+            if (navMeshAgent.isStopped == false)
+            {
+                isJump = true;
+                animator.SetBool("Jump", isJump);
+                animator.SetFloat("Move", 0);
+                navMeshAgent.isStopped = true;
+                jumpStartTime = Time.time;
+            }
+            else
+            {
+                if (jumpStartTime + jumpTime < Time.time)
+                {
+                    navMeshAgent.CompleteOffMeshLink();
+                    isJump = false;
+                    animator.SetBool("Jump", isJump);
+                    yield break;
+                }
+
+                var timeDelta = (Time.time - jumpStartTime) / jumpTime;
+                var newPostion = Vector3.Lerp(startPos, endPos, timeDelta);
+                newPostion.y += Mathf.Sin(timeDelta * Mathf.PI) * jumpHeight;
+                transform.position = newPostion;
+            }
+            yield return null;
         }
     }
 
